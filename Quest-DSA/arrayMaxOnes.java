89364f5bf0e7708946aba052cb860f2048434e59
Maneesh Reddy <77800380+maneesh18@users.noreply.github.com>
2025-12-18 09:57:30 -0600

    Create arrayMaxOnes.java

---
+++ Quest-DSA/arrayMaxOnes.java
@@ -0,0 +1,20 @@
+/*
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+*/
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max =0;
+        int count=0;
+        for(int i: nums){
+            if(i==1)
+                count++;
+            else{
+                max = Math.max(max,count);
+                count=0;
+            }
+        }
+        max = Math.max(max,count);
+        return max;
+    }
+}
