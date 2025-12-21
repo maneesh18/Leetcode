278befaf0548c696d75f176ffc159eadc51698a0
Maneesh Reddy <77800380+maneesh18@users.noreply.github.com>
2025-12-21 12:04:36 -0600

    Create validMountain.java

---
+++ Quest-DSA/validMountain.java
@@ -0,0 +1,20 @@
+/*
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+*/
+
+class Solution {
+    public boolean validMountainArray(int[] A) {
+        int n = A.length, i = 0, j = n - 1;
+        while (i + 1 < n && A[i] < A[i + 1]) i++;
+        while (j > 0 && A[j - 1] > A[j]) j--;
+        return i > 0 && i == j && j < n - 1;
+    }
+}
