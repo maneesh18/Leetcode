d037b08de110369a6f74d614fea74b0fd64cd50f
Maneesh Reddy <77800380+maneesh18@users.noreply.github.com>
2026-02-26 18:43:44 -0600

    Create maxPathSum.java

---
+++ top-interview-150/maxPathSum.java
@@ -0,0 +1,14 @@
+class Solution {
+    int ans = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return ans;
+    }
+    private int helper(TreeNode root) {
+        if (root == null) return 0;
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        ans = Math.max(ans, root.val + left + right);
+        return root.val + Math.max(left, right);
+    }
+}
